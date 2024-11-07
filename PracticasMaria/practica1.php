70897804de185877cf37cafc172e282d06293934
Maria de Jesus Pulido Zamora <l21530346@cancun.tecnm.mx>
2024-11-06 20:53:18 -0500

    Add files via upload

---
+++ PracticasMaria/practica1.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+<title>PHP</title>
+</head>
+<body>
+<?php
+ECHO"Hola!Este es mi primer script en PHP <BR>";
+//SALE ALGO EN PANTALLA
+#COMENTARIO
+/*ESTO ES UN BLOQUE DE COMENTARIO
+1*/
+$vartext="Hola!Ahora estoy usando variebles para guardar texto<BR>";
+echo $vartext;
+$varnum1=5;
+$varnum2=6;
+$varres=$varnum1+$varnum2;
+echo"El resultado es $varnum1 y $varnum2=$varres";
+?>
+</body>
+</html>
